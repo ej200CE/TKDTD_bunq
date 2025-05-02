20696d0cbecb4bb5680afd2bb33fdc7faec45db1
ej200CE <ruban.km73@gmail.com>
2025-05-02 20:55:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Yoo damn', {name})
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
